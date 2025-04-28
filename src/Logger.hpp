43a891da4d96983e728d30ff8e93d5b23fd6c4fd
brano-san <branoverv10@bk.ru>
2025-04-28 22:31:35 +0300

    Add logger macros

--- src/Logger.hpp
+++ src/Logger.hpp
@@ -1,6 +1,19 @@
-﻿#ifndef LOGGER_HPP
-#define LOGGER_HPP
+﻿#ifndef LOGGER_LAUNCHER_HPP
+#define LOGGER_LAUNCHER_HPP
 
-#include <iostream>
+#include "Logger/src/CategorizedLogger.hpp"
+
+namespace logger {
+DEFINE_CAT_LOGGER_MODULE(CoreLauncher);
+}  // namespace logger
+
+#define LOG_TRACE_L3(name, message, ...) QUILL_LOG_TRACE_L3(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
+#define LOG_TRACE_L2(name, message, ...) QUILL_LOG_TRACE_L2(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
+#define LOG_TRACE_L1(name, message, ...) QUILL_LOG_TRACE_L1(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
+#define LOG_DEBUG(name, message, ...) QUILL_LOG_DEBUG(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
+#define LOG_INFO(name, message, ...) QUILL_LOG_INFO(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
+#define LOG_WARNING(name, message, ...) QUILL_LOG_WARNING(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
+#define LOG_ERROR(name, message, ...) QUILL_LOG_ERROR(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
+#define LOG_CRITICAL(name, message, ...) QUILL_LOG_CRITICAL(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
-﻿int main()
+﻿#include "Logger.hpp"
+
+#include <thread>
+#include <windows.h>
+
+int main()
 {
+    logger::s_CoreLauncherLogger;
+
+    LOG_INFO(Core, "Hello");
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    RaiseException(EXCEPTION_ACCESS_VIOLATION, 0, 0, nullptr);
     return 0;
 }
