4e9d32ee66f971f83389a7f7145cd90d467b9162
brano <branoverv10@bk.ru>
2025-04-28 20:29:35 +0300

    Initial commit

---
+++ src/LoggerLauncher.cpp
@@ -0,0 +1,12 @@
+﻿// LoggerLauncher.cpp: определяет точку входа для приложения.
+//
+
+#include "LoggerLauncher.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
