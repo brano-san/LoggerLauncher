3a285c8109724c8800bcef55ce73f7559226539f
brano-san <branoverv10@bk.ru>
2025-04-29 23:48:08 +0300

    Fix logger for template categories

--- src/Logger.hpp
+++ src/Logger.hpp
@@ -1,21 +1,21 @@
 ﻿#ifndef LOGGER_LAUNCHER_HPP
 #define LOGGER_LAUNCHER_HPP
 
+#include <GenEnum/src/GenEnum.h>
 #include <Logger/src/CategorizedLogger.hpp>
 
-#include <GenEnum/src/GenEnum.h>
-
 namespace logger {
-DEFINE_CAT_LOGGER_MODULE(CoreLauncher);
+GENENUM(uint8_t, CoreLauncherSource, Core, Test);
+DEFINE_CAT_LOGGER_MODULE(CoreLauncher, CoreLauncherSources);
 }  // namespace logger
 
-#define LOG_TRACE_L3(name, message, ...) QUILL_LOG_TRACE_L3(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
-#define LOG_TRACE_L2(name, message, ...) QUILL_LOG_TRACE_L2(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
-#define LOG_TRACE_L1(name, message, ...) QUILL_LOG_TRACE_L1(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
-#define LOG_DEBUG(name, message, ...) QUILL_LOG_DEBUG(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
-#define LOG_INFO(name, message, ...) QUILL_LOG_INFO(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
-#define LOG_WARNING(name, message, ...) QUILL_LOG_WARNING(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
-#define LOG_ERROR(name, message, ...) QUILL_LOG_ERROR(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
-#define LOG_CRITICAL(name, message, ...) QUILL_LOG_CRITICAL(GET_LOGGER(CoreLauncher, name), message, ##__VA_ARGS__)
+#define LOG_TRACE_L3(name, message, ...) CAT_LOG_TRACE_L3(CoreLauncher, CoreLauncherSources, name, message, ##__VA_ARGS__)
+#define LOG_TRACE_L2(name, message, ...) CAT_LOG_TRACE_L2(CoreLauncher, CoreLauncherSources, name, message, ##__VA_ARGS__)
+#define LOG_TRACE_L1(name, message, ...) CAT_LOG_TRACE_L1(CoreLauncher, CoreLauncherSources, name, message, ##__VA_ARGS__)
+#define LOG_DEBUG(name, message, ...) CAT_LOG_DEBUG(CoreLauncher, CoreLauncherSources, name, message, ##__VA_ARGS__)
+#define LOG_INFO(name, message, ...) CAT_LOG_INFO(CoreLauncher, CoreLauncherSources, name, message, ##__VA_ARGS__)
+#define LOG_WARNING(name, message, ...) CAT_LOG_WARNING(CoreLauncher, CoreLauncherSources, name, message, ##__VA_ARGS__)
+#define LOG_ERROR(name, message, ...) CAT_LOG_ERROR(CoreLauncher, CoreLauncherSources, name, message, ##__VA_ARGS__)
+#define LOG_CRITICAL(name, message, ...) CAT_LOG_CRITICAL(CoreLauncher, CoreLauncherSources, name, message, ##__VA_ARGS__)
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
-﻿#include "Logger.hpp"
+﻿#include <iostream>
+
+#include "Logger.hpp"
 
 #include <thread>
 #include <windows.h>
 
 int main()
 {
-    logger::s_CoreLauncherLogger;
+    try
+    {
+        LOG_TRACE_L3(Core, "Hello");
+        LOG_TRACE_L2(Core, "Hello");
+        LOG_TRACE_L1(Core, "Hello");
+        LOG_DEBUG(Core, "Hello");
+        LOG_INFO(Core, "Hello");
+        LOG_WARNING(Core, "Hello");
+        LOG_ERROR(Core, "Hello");
+        LOG_CRITICAL(Test, "Hello");
+    }
+    catch (const std::exception& ex)
+    {
+        std::cout << ex.what() << '\n';
+    }
 
-    LOG_INFO(Core, "Hello");
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     RaiseException(EXCEPTION_ACCESS_VIOLATION, 0, 0, nullptr);
     return 0;
