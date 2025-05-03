5c46ec0bb57f179b7c8157379d86afc3bdbe2128
brano-san <branoverv10@bk.ru>
2025-05-03 17:08:09 +0300

    Update logs examples

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,35 @@
 
 #include "Logger.hpp"
 
-#include "quill/Backend.h"
-#include "quill/Frontend.h"
-#include "quill/LogMacros.h"
-#include "quill/Logger.h"
-#include "quill/sinks/ConsoleSink.h"
-
-#include <thread>
 #include <windows.h>
 
 int main()
 {
     try
     {
-        LOG_TRACE_L3(Core, "Hello");
-        LOG_TRACE_L2(Core, "Hello");
-        LOG_TRACE_L1(Core, "Hello");
-        LOG_DEBUG(Core, "Hello");
-        LOG_INFO(Core, "Hello");
-        LOG_WARNING(Core, "Hello");
-        LOG_ERROR(Core, "Hello");
-        LOG_CRITICAL(Test, "Hello");
+        LOG_TRACE_L3(Core, "Core - LOG_TRACE_L3");
+        LOG_TRACE_L2(Core, "Core - LOG_TRACE_L2");
+        LOG_TRACE_L1(Core, "Core - LOG_TRACE_L1");
+        LOG_DEBUG(Core, "Core - LOG_DEBUG");
+        LOG_INFO(Core, "Core - LOG_INFO");
+        LOG_WARNING(Core, "Core - LOG_WARNING");
+        LOG_ERROR(Core, "Core - LOG_ERROR");
+        LOG_CRITICAL(Core, "Core - LOG_CRITICAL");
+
+        LOG_TRACE_L3(Test, "Test - LOG_TRACE_L3");
+        LOG_TRACE_L2(Test, "Test - LOG_TRACE_L2");
+        LOG_TRACE_L1(Test, "Test - LOG_TRACE_L1");
+        LOG_DEBUG(Test, "Test - LOG_DEBUG");
+        LOG_INFO(Test, "Test - LOG_INFO");
+        LOG_WARNING(Test, "Test - LOG_WARNING");
+        LOG_ERROR(Test, "Test - LOG_ERROR");
+        LOG_CRITICAL(Test, "Test - LOG_CRITICAL");
     }
     catch (const std::exception& ex)
     {
         std::cout << ex.what() << '\n';
     }
 
-    // QUILL_LOG_BACKTRACE(logger::s_CoreLauncherLogger.getLogger(logger::CoreLauncherSources::kCore), "Backtrace log {}", 100);
-
-    // std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     // RaiseException(EXCEPTION_ACCESS_VIOLATION, 0, 0, nullptr);
 
     LOG_INFO(Core, "BEFORE backtrace Example {}", 1);
@@ -41,28 +40,18 @@ int main()
     QUILL_LOG_BACKTRACE(logger::s_CoreLauncherLogger.getLogger(logger::CoreLauncherSources::kCore), "Backtrace log {}", 3);
     QUILL_LOG_BACKTRACE(logger::s_CoreLauncherLogger.getLogger(logger::CoreLauncherSources::kCore), "Backtrace log {}", 4);
 
-    // Backtrace is not flushed yet as we requested to flush on errors
     LOG_INFO(Core, "AFTER backtrace Example {}", 1);
 
-    // log message with severity error - This will also flush_sink the backtrace which has 2 messages
     LOG_ERROR(Core, "An error has happened, Backtrace is also flushed.");
-
-    // The backtrace is flushed again after LOG_ERROR but in this case it is empty
     LOG_ERROR(Core, "An second error has happened, but backtrace is now empty.");
 
-    // Log more backtrace messages
     QUILL_LOG_BACKTRACE(
         logger::s_CoreLauncherLogger.getLogger(logger::CoreLauncherSources::kCore), "Another Backtrace log {}", 1);
     QUILL_LOG_BACKTRACE(
         logger::s_CoreLauncherLogger.getLogger(logger::CoreLauncherSources::kCore), "Another Backtrace log {}", 2);
 
-    // Nothing is logged at the moment
     LOG_INFO(Core, "Another log info");
-
-    // Still nothing logged - the error message is on a different logger object
     LOG_CRITICAL(Core, "A critical error from different logger.");
-
-    // The new backtrace is flushed again due to LOG_CRITICAL
     LOG_CRITICAL(Core, "A critical error from the logger we had a backtrace.");
 
     return 0;
